0eeec663a53ba71e1b4fa804cbfddca7433766d2
bill <>
2019-01-05 11:11:42 +0500

    authorize token update

--- etc/generateToken.js
+++ etc/generateToken.js
@@ -5,4 +5,9 @@ function createToken(params) {
         expiresIn: parseInt(process.env.JWT_TOKEN_EXPIRY_TIME)});
     return Promise.resolve(token);
 }
+
+function authorizeToken(token) {
+    let verifiedTotken = jwt.verify(token, process.env.SECRET); 
+    return verifiedTotken;
+}
 module.exports = { createToken };
